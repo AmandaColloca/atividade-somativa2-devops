b1b051bb22d4ff76efd8b526132509d3e448f53a
AmandaColloca <117382027+AmandaColloca@users.noreply.github.com>
2023-04-24 18:06:09 -0300

    Update telegram-message.js

--- telegram-message.js
+++ telegram-message.js
@@ -12,4 +12,4 @@ function sendMessage(message) {
 }
 
 
-sendMessage('Teste: Aviso de uma nova PR aberta no seu repositório GitHub');
+sendMessage('Teste2: Aviso de uma nova PR aberta no seu repositório GitHub');
